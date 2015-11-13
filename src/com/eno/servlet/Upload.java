3b47620b22ba232e6ed90998c33214794505817d
genophy_lap <genophy@gmail.com>
2015-11-13 13:41:51 -0200

    first commit

---
+++ src/com/eno/servlet/Upload.java
@@ -0,0 +1,79 @@
+package com.eno.servlet;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+ 
+/**
+ * Servlet implementation class Upload
+ */
+@WebServlet("/Upload")
+public class Upload extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Upload() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		 	String savePath = "/home/eno/mypro/uploads/";
+
+		File f1 = new File(savePath);
+		if (!f1.exists()) {
+			f1.mkdirs();
+		}
+		DiskFileItemFactory fac = new DiskFileItemFactory();
+		ServletFileUpload upload = new ServletFileUpload(fac);
+		upload.setHeaderEncoding("utf-8");
+		List<FileItem> fileList = null;
+		try {
+			fileList = upload.parseRequest(request);
+		} catch (FileUploadException ex) {
+			return;
+		}
+		Iterator<FileItem> it = fileList.iterator();
+		while (it.hasNext()) {
+			FileItem item = it.next();
+			if (!item.isFormField()) {
+				try {
+					item.write(new File(savePath + "/"+ item.getName()));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		response.getWriter().print("success" );
+	}  
+		
+		
+
+}
